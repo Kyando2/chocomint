69301689a2a3c47eafd9721cca5dc7f2f9889962
キャンド <34218031+Kyando2@users.noreply.github.com>
2020-08-19 04:48:29 +0900

    Added possible improvement

--- src/internals/regex.rs
+++ src/internals/regex.rs
@@ -1,8 +1,13 @@
-use regex::Regex;
+//! Returns a string matching the predefined constant regex
 
+use regex::Regex;
+/* Possible other regex: 
+"(?<=Roblox.XrsfToken.setToken\\(')(.+?)(?='\\))" => Rust version 
+"(?<=Roblox\.XsrfToken\.setToken\(')(.+?)(?='\))" => Javascript version
+*/
 /// Finds the x-csrf-token in the body and returns it as an owned string
 pub fn match_token(text: &str) -> String {
-    let re = Regex::new("Roblox.XsrfToken.setToken\\('(.+?)'\\)")
+    let re = Regex::new("Roblox.XsrfToken.setToken\\('(.+?)'\\)") 
         .unwrap();
     let re2 = Regex::new("'(.+?)'")
         .unwrap();
