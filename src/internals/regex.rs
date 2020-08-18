5b050a765f6c2fb048c73d743075df588e7559cd
キャンド <34218031+Kyando2@users.noreply.github.com>
2020-08-19 06:31:18 +0900

    Update regex.rs

--- src/internals/regex.rs
+++ src/internals/regex.rs
@@ -1,9 +1,15 @@
 //! Returns a string matching the predefined constant regex
 
 use regex::Regex;
-/* Possible other regex: 
+/* 
+Possible other regex: 
+# 1, Lookarounds;
 "(?<=Roblox.XrsfToken.setToken\\(')(.+?)(?='\\))" => Rust version 
 "(?<=Roblox\.XsrfToken\.setToken\(')(.+?)(?='\))" => Javascript version
+---
+# Multiple captures;
+"(Roblox.XrsfToken.setToken\\(')(.+?)('\\))" => Rust version 
+"(Roblox\.XsrfToken\.setToken\(')(.+?)('\))" => Javascript version
 */
 /// Finds the x-csrf-token in the body and returns it as an owned string
 pub fn match_token(text: &str) -> String {
